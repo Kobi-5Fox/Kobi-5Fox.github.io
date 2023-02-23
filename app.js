8ca7fea5a019e00209c0be95bb0a836dead232bc
Kobinathan T <intern-fox-01@pentafox.in>
2023-02-23 14:33:05 +0530

    Added few end points

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express');
 
 const app = express();
 const PORT = 4200;
+app.use(express.json());
+
 
 app.get('/', (req, res)=>{
     const path=require('path');
     res.status(200);
     res.send(`<h2>Welcome to my Test App<h2>`);
 });
-app.use(express.json());
-app.post('/', (req, res)=>{
+
+app.post('/purchased', (req, res)=>{
     res.status(200);
     res.send(`Headers ${JSON.stringify(req.headers)}, \r\n  Body :${JSON.stringify(req.body)}`);
 });
+
+app.get('/trial', (req, res)=>{
+    res.status(200);
+    res.send('Trial Plan is Under Testing');
+});
+
+app.get('/security-policy', (req, res)=>{
+    res.status(200);
+    res.send('We are committed to protect the customers data');
+});
+
 
 app.listen(PORT, (error) =>{
 	if(!error)
